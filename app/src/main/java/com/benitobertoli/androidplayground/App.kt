e7b28a59684aef26d758aa185bddafbbdcbbb19e
Benito Bertoli <binoprod@gmail.com>
2020-10-21 02:47:56 +0300

    setup themes

--- app/src/main/java/com/benitobertoli/androidplayground/App.kt
+++ app/src/main/java/com/benitobertoli/androidplayground/App.kt
@@ -1,5 +1,6 @@
 package com.benitobertoli.androidplayground
 
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDex
 import com.benitobertoli.androidplayground.di.DaggerAppComponent
 import dagger.android.AndroidInjector
@@ -10,6 +11,7 @@ class App : DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
         MultiDex.install(this)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
